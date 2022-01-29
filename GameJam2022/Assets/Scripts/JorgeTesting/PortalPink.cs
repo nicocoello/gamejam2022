09a3750075c30b837da699724168f3b38c971767
Shorsho <78431489+Shorsho@users.noreply.github.com>
2022-01-29 18:42:58 -0300

    Portales pink y blue

---
+++ GameJam2022/Assets/Scripts/JorgeTesting/PortalPink.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalPink : MonoBehaviour
+{
+    public GameObject BluePortal;
+    public GameObject PinkPortal;
+    public GameObject BluePlayer;
+
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            StartCoroutine(transport());
+        }
+    }
+
+    IEnumerator transport()
+    {
+        yield return new WaitForSeconds(0.05f);
+        BluePlayer.transform.position = new Vector2(BluePortal.transform.position.x, BluePortal.transform.position.y);
+    }
+    
+}
