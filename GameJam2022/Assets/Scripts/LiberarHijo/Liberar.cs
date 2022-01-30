f3070b6f901a43aa26f54719a6c966ebb73f441b
Maxi <tholem98@gmail.com>
2022-01-30 14:57:37 -0300

    escena del pasado LISTA

--- GameJam2022/Assets/Scripts/LiberarHijo/Liberar.cs
+++ GameJam2022/Assets/Scripts/LiberarHijo/Liberar.cs
@@ -8,13 +8,16 @@ public class Liberar : MonoBehaviour
     public bool player2 = false;
     public bool key1 = false;
     public bool key2 = false;
-    public GameObject keyState;
+    public GameObject keyState=null;
 
     // Start is called before the first frame update
     void Start()
     {
-        key1 = keyState.gameObject.GetComponent<EstadoLlaves>().key1;
-        key2 = keyState.gameObject.GetComponent<EstadoLlaves>().key2;
+        if (keyState)
+        {
+            key1 = keyState.gameObject.GetComponent<EstadoLlaves>().key1;
+            key2 = keyState.gameObject.GetComponent<EstadoLlaves>().key2;
+        }
 
     }
 
